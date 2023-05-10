2f67dc889548d0aa7dec2aaa33f637f346954785
ici <ici@DESKTOP-P502TBK>
2023-05-10 12:07:24 +0900

    springboot JPA 연습 예제

--- src/main/java/com/moohee/jpa/repository/MemberRepository.java
+++ src/main/java/com/moohee/jpa/repository/MemberRepository.java
@@ -12,6 +12,10 @@ public interface MemberRepository extends JpaRepository<MemberDto, Long>{
 	
 	public List<MemberDto> findByName(String name);
 	public List<MemberDto> findAllByOrderByHakbunDesc(); //학번 내림차순으로 정렬 후에 모든 리스트 가져오기
+	public List<MemberDto> findByNameStartingWith(String nameKeyword); //이름에서 특정단어로 시작하는 회원 찾기
+	public List<MemberDto> findByNameEndingWith(String nameKeyword); //이름에서 특정단어로 끝나는 회원 찾기
+	public List<MemberDto> findByNameLike(String nameKeyword); //이름에서 특정단어가 포함된 회원 찾기
+	
 	
 	@Transactional
 	public void deleteAllByName(String name); //이름으로 조회하여 삭제
--- src/test/java/com/moohee/jpa/JpaTest.java
+++ src/test/java/com/moohee/jpa/JpaTest.java
@@ -1,7 +1,6 @@
 package com.moohee.jpa;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -52,25 +51,49 @@ public class JpaTest {
 //		}
 //	}
 	
+//	@Test
+//	@DisplayName("회원 정보 수정 테스트")
+//	public void modifyMember() {
+//		
+//		Optional<MemberDto> optionalDto = memberRepository.findById(10L);
+//		
+//		if(optionalDto.isPresent()) { //null 값 여부 체크
+//		
+//			MemberDto memberDto = optionalDto.get();
+//			
+//			memberDto.setAge(32); //나이수정
+//			memberDto.setName("이순신"); //이름수정
+//			
+//			memberRepository.save(memberDto);
+//		}
+//		
+//		optionalDto = memberRepository.findById(3L);
+//		
+//		System.out.println(optionalDto.get().toString());
+//		
+//	}
+	
+//	@Test
+//	@DisplayName("회원 이름 중 성씨로 찾기")
+//	public void searchFirst() {
+//		
+//		List<MemberDto> memberDtos = memberRepository.findByNameStartingWith("강");
+//		
+//		for(MemberDto memberDto : memberDtos) {
+//			System.out.println(memberDto.toString());
+//		}
+//		
+//	}
+	
 	@Test
-	@DisplayName("회원 정보 수정 테스트")
-	public void modifyMember() {
+	@DisplayName("회원 이름 중 특정단어 포함된 회원 찾기")
+	public void searchFirst2() {
 		
-		Optional<MemberDto> optionalDto = memberRepository.findById(10L);
+		List<MemberDto> memberDtos = memberRepository.findByNameLike("%가%");
 		
-		if(optionalDto.isPresent()) { //null 값 여부 체크
-		
-			MemberDto memberDto = optionalDto.get();
-			
-			memberDto.setAge(32); //나이수정
-			memberDto.setName("이순신"); //이름수정
-			
-			memberRepository.save(memberDto);
+		for(MemberDto memberDto : memberDtos) {
+			System.out.println(memberDto.toString());
 		}
-		
-		optionalDto = memberRepository.findById(3L);
-		
-		System.out.println(optionalDto.get().toString());
 		
 	}
 
